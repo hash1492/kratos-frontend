820a15b9cdc91daef557295bdeb27c79b8217e35
Harshil Shah <hash_1492@yahoo.com>
2018-05-13 02:46:34 -0400

    added guard to routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,8 @@ import MyProfile from '../components/MyProfile.vue'
 import ViewOthersProfile from '../components/ViewOthersProfile.vue'
 import FriendsList from '../components/FriendsList.vue'
 
+import Auth from '../services/auth/auth.service'
+
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -17,42 +19,96 @@ export default new VueRouter({
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
+      beforeEnter: (to, from, next) => {
+        if(Auth.isLoggedIn()) {
+          next({name: 'Home'});
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
+      beforeEnter: (to, from, next) => {
+        if(Auth.isLoggedIn()) {
+          next({name: 'Home'});
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/forgot-password',
       name: 'ForgotPassword',
-      component: ForgotPassword
+      component: ForgotPassword,
+      beforeEnter: (to, from, next) => {
+        if(Auth.isLoggedIn()) {
+          next({name: 'Home'});
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/reset-password',
       name: 'ResetPassword',
-      component: ResetPassword
+      component: ResetPassword,
+      beforeEnter: (to, from, next) => {
+        if(Auth.isLoggedIn()) {
+          next({name: 'Home'});
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/home',
       name: 'Home',
-      component: Home
+      component: Home,
+      beforeEnter: (to, from, next) => {
+        if(!Auth.isLoggedIn()) {
+          next({name: 'Login'});
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/my-profile',
       name: 'MyProfile',
-      component: MyProfile
+      component: MyProfile,
+      beforeEnter: (to, from, next) => {
+        if(!Auth.isLoggedIn()) {
+          next({name: 'Login'});
+        }
+      }
     },
     {
       path: '/profile/:user_id',
       name: 'ViewOthersProfile',
-      component: ViewOthersProfile
+      component: ViewOthersProfile,
+      beforeEnter: (to, from, next) => {
+        if(!Auth.isLoggedIn()) {
+          next({name: 'Login'});
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/my-friends',
       name: 'FriendsList',
-      component: FriendsList
+      component: FriendsList,
+      beforeEnter: (to, from, next) => {
+        if(!Auth.isLoggedIn()) {
+          next({name: 'Login'});
+        } else {
+          next();
+        }
+      }
     },
     { path: '*', redirect: '/login' }
   ]
